edc9792b50097bad8f84c089d3766ea5359013c9
shayanx45 <shayanx45@gmail.com>
2016-09-23 11:54:43 -0400

    TestLoops

---
+++ TestLoops.java
@@ -0,0 +1,12 @@
+public class TestLoops{
+public static void main(String[] args){
+
+    System.out.println("Hello");
+    
+        System.out.println("For the loop");
+    
+    for(int index = 0; index < 6; index++){
+            System.out.println("Hello");
+        }
+    }
+}
